83118eb57bbc72f7b8635f21a93eb52ae80d797c
n3cs1s <pavel.primavera@gmail.com>
2017-03-05 15:35:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $arr_fields=range(1,$field_count);
 $num_arr=array_rand($arr_fileds,1);
 //$num=$num_arr[];
 print_r($arr_fileds);
-echo $filed_count."<br />";
+echo "Field_count - ".$filed_count.";<br />";
 
 $sql = "SELECT * FROM ".$table." WHERE id=".$num;
 
