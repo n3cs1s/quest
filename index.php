7474e90cd21f4facbb10c23f28383463b4442d55
n3cs1s <pavel.primavera@gmail.com>
2017-03-05 15:13:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,10 +19,13 @@ $count=$connect->query("SELECT COUNT(*) FROM ".$table);
 //Вибирає останню додану загадкку, для вибору випадкової - створити $arr_fields=range(1,$field_count), потім $num=array_rand($array_fileds,1);
 
 $num=1;
+while($cnt = $count->fetch_assoc()) {
+        $field_count=$cnt["field_count"];
+    }
 /*$arr_fields=range(1,$field_count);
 $num_arr=array_rand($arr_fileds,1);*/
 //$num=$num_arr[];
-print_r($count);
+//print_r($count);
 
 $sql = "SELECT * FROM ".$table." WHERE id=".$num;
 
