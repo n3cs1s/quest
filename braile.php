f8371cfed41441b92bea8674b74848e74f2c5e46
n3cs1s <pavel.primavera@gmail.com>
2017-03-24 18:36:48 +0200

    Update braile.php

--- braile.php
+++ braile.php
@@ -4,6 +4,8 @@ header('Content-Type: text/html; charset=utf-8');
 <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
 
 <body>
+  <div id="target">Nothing
+  </div>
 <form id="formWithFiles" method="POST" enctype="multipart/form-data">
 <input type="file" name="file">
 <input type="submit" value="submit" title="submit">
@@ -16,6 +18,7 @@ $.ajax({
   data: new FormData($('#formWithFiles')[0]), // The form with the file inputs.
   processData: false                          // Using FormData, no need to process data.
 }).done(function(){
+  $("#target").html(data);
   console.log("Success: Files sent!");
 }).fail(function(){
   console.log("An error occurred, the files couldn't be sent!");
