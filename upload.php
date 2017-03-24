14e444cbb9f956e6e8b87abcbfec46995d7eb85c
n3cs1s <pavel.primavera@gmail.com>
2017-03-24 19:13:46 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 $upload='';
 if(isset($_FILES["file"]["name"])){
   $upload= $_FILES["file"]["name"];
-  $content=file_get_contents($_FILES['file']['tmp_name']);
+  $content=nl2br(file_get_contents($_FILES['file']['tmp_name']));
 }
 echo $content;
 ?>
