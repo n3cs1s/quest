095ee5c73d4ad776eb36c1267854d357fac54242
n3cs1s <pavel.primavera@gmail.com>
2017-03-24 18:20:13 +0200

    Create braile.php

---
+++ braile.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
+
+<body>
+<form id="formWithFiles">
+<input type="file" name="file"></input>
+<input type="submit" value="submit" title="submit">
+</form>
+
+<script>
+$.ajax({
+  url: 'destination.txt', 
+  type: 'POST',
+  data: new FormData($('#formWithFiles')[0]), // The form with the file inputs.
+  processData: false                          // Using FormData, no need to process data.
+}).done(function(){
+  console.log("Success: Files sent!");
+}).fail(function(){
+  console.log("An error occurred, the files couldn't be sent!");
+});
+</script>
