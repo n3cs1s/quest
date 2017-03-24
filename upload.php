2697f3d7be64008b99486b293e05b33339a033d3
n3cs1s <pavel.primavera@gmail.com>
2017-03-24 21:04:46 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,13 @@
 <?php
 $upload='';
+$filename="";
 if(isset($_FILES["file"]["name"])){
   $upload= $_FILES["file"]["name"];
   $content=nl2br(file_get_contents($_FILES['file']['tmp_name']));
+  $filename=$_FILES['file']['tmp_name'];
 }
-echo $content;
+//echo $content;
+$content ="";
+exec("lou_translate -b sk.tbl $filename",$content);
+print_r($content);
 ?>
